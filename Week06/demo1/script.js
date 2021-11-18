6520addaf48cc0f965fc658f57202c844aafaa79
Mui Hoon <omh@np.edu.sg>
2021-11-18 15:19:48 +0800

    week 6 demo

---
+++ Week06/demo1/script.js
@@ -0,0 +1,7 @@
+let quantity = 3;
+let price = 1.5;
+let totalCost = quantity * price;
+console.log('Quantity = ' + quantity);
+console.log('Price = ' + price);
+console.log('Total Cost = ' + totalCost);
+
